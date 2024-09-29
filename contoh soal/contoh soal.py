f3f87b3a2b13ce203892e9c908af1c4166b63651
arunikasky <shuinorix1@gmail.com>
2024-09-29 19:52:39 +0700

    Add files via upload

---
+++ contoh soal/contoh soal.py
@@ -0,0 +1,6 @@
+# w = mg
+w = 25.6 #N
+g = 9.8 #N/Kg
+# m = w/g
+m = w / g
+print(" Massa : ",m, "Kg")
---
+++ soal 1/soal 1.py
@@ -0,0 +1,8 @@
+# mengitung fokus lensa (f = 1 / ((n - 1) * (1/R1 + 1/R2)))
+n = 1.50
+R1 = 22
+R2 = 17.5
+
+#fokus lensa
+f = 1 / ((n - 1) * (1/R1 + 1/R2))
+print("panjang fokus (f) : ",f, "cm")
